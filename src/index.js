019d77b734a79c7696da9d560c401c4b8d556d5d
bibek977 <bhattaraibibek91@gmail.com>
2022-01-10 18:28:34 +0545

    greeting function add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const timeNow = new Date().toLocaleTimeString()
-
+const timeNow = new Date(2022, 1, 10, 14)
+const hour = timeNow.getHours()
+let greet = ''
+let myStyle = ''
+const center = {
+    display : 'flex',
+    justifyContent : 'center',
+    alignItems : 'center'
+}
+if(hour>=0 && hour <12){
+     greet = 'morning'
+     myStyle = {
+         color : 'red'
+     }
+}
+else if (hour>=12 && hour <18){
+    greet = "afternoon"
+    myStyle = {
+        color : 'blue'
+    }
+}
+else{
+    greet = 'evening'
+    myStyle = {
+        color : 'green'
+    }
+}
 ReactDOM.render(
     <>
-    <h1 contentEditable="true">Time : {timeNow} </h1>
+    <h1 style={center}>Good, <span style={myStyle}> {greet} </span></h1>
     </>,
  
     document.getElementById('root')

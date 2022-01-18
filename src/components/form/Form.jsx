098281d5a959f5e1e48c4f685b36b94216f15626
bibek977 <bhattaraibibek91@gmail.com>
2022-01-18 11:33:24 +0545

    input field added

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 import '../sass/form/form.css'
 
 export default function Form() {
+    const [initText, finalText]= useState()
+    const [myName, fullName]=useState()
+    
+    const inputText=(event)=>{
+        finalText(event.target.value)
+    }
+    const onSubmit=()=>{
+        fullName(initText)
+    }
     return (
         <div className='form'>
-            <h1>hello</h1>
-            <input type="text" />
+            <h1>hello {myName}</h1>
+            <input type="text"  onChange={inputText} value={initText}/>
+            <button onClick={onSubmit}>Click Me</button>
         </div>
     )
 }

6624ebda03028201f2b94ff3c6df8f0a18a50d6e
bibek977 <bhattaraibibek91@gmail.com>
2022-01-18 13:54:37 +0545

    form improved

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
-import { useState } from 'react/cjs/react.development'
+import React,{useState} from 'react'
 import '../sass/form/form.css'
 
 export default function Form() {
-    const [initText, finalText]= useState()
-    const [myName, fullName]=useState()
-    
-    const inputText=(event)=>{
-        finalText(event.target.value)
+
+    const [fullId, setfullId] = useState('')
+    const [fullPw, setfullPw] = useState('')
+    const [initText, setinitText] = useState( ()=>{
+        return{
+            id : '',
+            pw : '' 
+        }
+    } )
+    const inputEvent=(event)=>{
+        const user = event.target.value
+        const userName = event.target.name
+        setinitText((preValue)=>{
+
+                if(userName==='id'){
+                    return{
+
+                        id : user,
+                        pw : preValue.pw
+                    }
+                } 
+                else if (userName === 'pw'){
+                    return{
+
+                        id : preValue.id,
+                        pw : user
+                    }
+                }
+                
+        })
     }
-    const onSubmit=()=>{
-        fullName(initText)
+    const result=(event)=>{
+        event.preventDefault()
+        setfullId(initText.id)
+        setfullPw(initText.pw)
     }
     return (
-        <div className='form'>
-            <h1>hello {myName}</h1>
-            <input type="text"  onChange={inputText} value={initText}/>
-            <button onClick={onSubmit}>Click Me</button>
+        <div className='form' onSubmit={result}>
+            <form >
+                <h1>Id : {fullId}</h1>
+                <h1>Pw : {fullPw}</h1>
+                <input type="text" name="id" onChange={inputEvent} value = {initText.id}/>
+                <input type="password" name="pw" onChange={inputEvent} value={initText.pw}/>
+
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
